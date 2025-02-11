20ff6cb0234f33e8ce12c728ce46ade298fd8d90
Andres <carrilloandres128@gmail.com>
2025-02-10 22:27:57 -0500

    Se conecta al array de palabras para generar de manera aleatoria

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
+import { words as INITIAL_WORDS } from './words.js'
 const $time = document.querySelector('time');
 const $paragraph = document.querySelector('p');
 const $input = document.querySelector('input');
 
 const INITIAL_TIME = 30;
-
-const TEXT = 'lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit similique aperiam reiciendis rerum facilis nisi eveniet, quos inventore quam optio laudantium blanditiis. Harum quis quia explicabo eos praesentium, dolorum velit!'
 
 let words = [];
 let currentTime = INITIAL_TIME;
@@ -13,8 +12,10 @@ initGame();
 initEvents();
 
 function initGame() {
-  const textContent = $paragraph.textContent;
-  words = TEXT.split(' ').slice(0, 32);
+  words = INITIAL_WORDS.toSorted(
+    () => Math.random() - 0.5
+  ).slice(0, 32);
+  
   currentTime = INITIAL_TIME;
 
   $time.textContent = currentTime;
